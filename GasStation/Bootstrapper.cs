2979e2efef27efe098919ed3b4001afab7944f0a
Fenix506 <marian.bartnovskyi@gmail.com>
2018-04-19 02:56:42 +0300

    Start project for course work

---
+++ GasStation/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using GasStation.ViewModels;
+
+namespace GasStation
+{
+  public class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
